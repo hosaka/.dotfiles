1f255517bf6530595c461244675c082597c1b327
Alex March <alexmarch@fastmail.com>
2025-02-25 13:39:43 +0900

    feat(pwsh): add mise and a bunch of tools that it introduces

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,11 +1,42 @@
 $ProfileDir = Split-Path $Profile.CurrentUserAllHosts -Parent
-$MachineProfile = Join-Path "$ProfileDir" "$($(hostname).ToLower()).profile.ps1"
+$MachineProfile = (hostname).ToLower()
 
 . (Join-Path $ProfileDir "functions.ps1")
-. (Join-Path $ProfileDir "aliases.ps1")
 
-# load machine profiles
-if (Test-Path $MachineProfile) {
-  . $MachineProfile
+# mise
+if (Has "mise") {
+  $env:MISE_ENV = $MachineProfile
+  mise activate pwsh | Out-String | Invoke-Expression
+  # mise hook-env -s pwsh | Out-String | Invoke-Expression
 }
 
+# alias definitions (uses tools from cargo and mise)
+. (Join-Path $ProfileDir "aliases.ps1")
+
+# starship prompt
+if (Has "starship") {
+  Invoke-Expression (&starship init powershell)
+}
+
+# zoxide
+if (Has "zoxide") {
+  zoxide init powershell | Out-String | Invoke-Expression
+}
+
+# just
+if (Has "just") {
+  just --completions powershell | Out-String | Invoke-Expression
+}
+
+# delta
+if (Has "delta") {
+  $env:GIT_PAGER = "delta"
+  $env:DELTA_FEATURES = "side-by-side line-numbers navigate"
+  # todo: not sure how to add interactive.diffFilter = delta --color-only
+}
+
+# load machine profiles
+$MachineProfileScript = Join-Path "$ProfileDir" "$MachineProfile.profile.ps1"
+if (Test-Path $MachineProfileScript) {
+  . $MachineProfileScript
+}
