107a3f6cf1236bc081d42b5292a7e5362f0bbd67
Alex March <alexmarch@fastmail.com>
2026-03-26 16:25:14 +0900

    fix(chezmoi): add shared script and shared utils templates

--- dotfiles/.chezmoitemplates/shared_functions.bash
+++ dotfiles/.chezmoitemplates/shared_functions.bash
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 BOLD="$(tput bold 2>/dev/null || printf '')"
 GREY="$(tput setaf 0 2>/dev/null || printf '')"
 UNDERLINE="$(tput smul 2>/dev/null || printf '')"
@@ -64,6 +62,7 @@ onpath() {
 # source if exists
 include() {
   if [ -e "$*" ]; then
+    # shellcheck disable=SC1090 # arbitrary include
     . "$*"
   fi
 }
@@ -84,60 +83,3 @@ which_sudo() {
 on_wsl() {
   test -n "${WSL_DISTRO_NAME}"
 }
-
-# create a new directory and enter it
-function mkd() {
-  mkdir -p "$@" && cd "$_" || return
-}
-
-# go up until a file or dir is found
-function cdu() {
-  local start=$PWD
-  local target=${*:-.git}
-  while ! { test -f "$target" || test -d "$target"; }; do
-    cd ..
-    if [ "$PWD" = "/" ]; then
-      warning "nothing found" >&2
-      cd "$start" || return 1
-      pwd
-      return 1
-    fi
-  done
-  pwd
-  return 0
-}
-
-if has gpg; then
-  function secret() {
-    local filename=${1}
-    if ! test -f "${filename}"; then
-      warning "provide a file to encrypt" >&2
-      return 1
-    fi
-    local output="$filename.enc"
-    gpg --armor --symmetric --output "${output}" "${filename}"
-    return 0
-  }
-
-  function reveal() {
-    local filename=${1}
-    if ! test -f "${filename}"; then
-      warning "provide a file to decrypt" >&2
-      return 1
-    fi
-    local output=$(basename "${filename}" ".enc")
-    gpg --decrypt --output "${output}" "${filename}"
-    return 0
-  }
-fi
-
-if has yazi; then
-  function y() {
-    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-    yazi "$@" --cwd-file="$tmp"
-    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-      builtin cd -- "$cwd"
-    fi
-    rm -f -- "$tmp"
-  }
-fi
