91d60cae163e00e2f3a0faabd9fc8fda47eb048f
Alex March <alexmarch@fastmail.com>
2026-03-31 13:12:08 +0900

    fix(fish): lazy load zoxide

--- dotfiles/dot_config/private_fish/conf.d/zoxide.fish
+++ dotfiles/dot_config/private_fish/conf.d/zoxide.fish
@@ -1,3 +1,15 @@
 if status is-interactive; and has zoxide
-  eval "$(zoxide init fish)"
+  function __lazy_zoxide
+    functions -e __lazy_zoxide z zi
+    # only runs once
+    zoxide init fish | source
+  end
+  function z -d 'zoxide (lazy)'
+    __lazy_zoxide
+    __zoxide_z $argv
+  end
+  function zi -d 'zoxide interactive (lazy)'
+    __lazy_zoxide
+    __zoxide_zi $argv
+  end
 end
